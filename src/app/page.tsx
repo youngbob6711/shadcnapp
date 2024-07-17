9875f2f747fc537cbf7cffa70938476162383809
unknown <youngbob6711@gmail.com>
2024-07-17 20:57:45 +0300

    darkmode included

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
 import { CardWithForm } from "@/components/CardwithForm";
+import { DarkModeToggler } from "@/components/DarkmodeToggler";
 import { NavigationMenuDemo } from "@/components/NavBar";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
+    
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <DarkModeToggler/>
       <NavigationMenuDemo/>
      <Button>Sign Up</Button>
      <CardWithForm/>
+     
      
     </main>
   );
